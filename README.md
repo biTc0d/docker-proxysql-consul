dd7dad329a826bc7d5d5d17702e9cc4b2be2d868
biTc0d <ciprian@comarita.com>
2019-01-29 15:01:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+ProxySQL docker image with Consul support
+===================================
+
+The docker image is available right now at `bitc0d/proxysql-consul:latest`.
+
+[See it on Docker Hub](https://hub.docker.com/r/bitc0d/proxysql-consul)
+
+Basic usage
+-----------
+
+docker run -d bitc0d/proxysql-consul:latest add_cluster_nodes.sh
+
+The Docker image accepts the following parameters (all are mandatory):
+* `MYSQL_ROOT_PASSWORD`
+* `MYSQL_PROXY_USER`
+* `MYSQL_PROXY_PASSWORD`
+* `PXC_SERVICE`
---
+++ proxysql-entry.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ -z "$PXC_SERVICE" ]; then
+        echo >&2 'Error:  You need to specify PXC_SERVICE'
+        exit 1
+fi
+
+if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+        echo >&2 'Error:  You need to specify MYSQL_ROOT_PASSWORD'
+        exit 1
+fi
+
+if [ -z "$MYSQL_PROXY_USER" ]; then
+        echo >&2 'Error:  You need to specify MYSQL_PROXY_USER'
+        exit 1
+fi
+
+if [ -z "$MYSQL_PROXY_PASSWORD" ]; then
+        echo >&2 'Error:  You need to specify MYSQL_PROXY_PASSWORD'
+        exit 1
+fi
+
+/usr/bin/proxysql --initial -f -c /etc/proxysql.cnf 
